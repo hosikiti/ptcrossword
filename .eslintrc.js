7a1c3f0124f912e28584cdab34b06e7008f23f05
Kei Oikawa <hosikiti@gmail.com>
2025-04-02 00:14:23 +0100

    Fix ESLint configuration for deployment

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: ['react-app'],
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // Add custom rules here if needed
+  },
+}; 
